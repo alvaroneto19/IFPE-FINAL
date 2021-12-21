f42e42dbfa31185a1b7af5a5ff73fb0e9534e163
alvaroneto19 <81449169+alvaroneto19@users.noreply.github.com>
2021-12-20 21:29:13 -0300

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,30 @@
+package biblioteca;
+
+
+public interface Interface {
+    static void adicionar(){      
+   }
+    static  double analisedeestoque(){
+       return 0; 
+   }
+       
+   static  public String ver(){
+       return "";
+   }
+   
+   static int verificar(){
+        return 0;
+   
+   }    
+   static int verificar(double começo, double fim){
+        return 0;
+   }
+   
+  static  boolean retirar(String titulo){
+        return false;   
+  }
+   static void adicionar(Livraria nLivros){
+       
+   }
+   
+}
