5e11ad330af78b582c9b9bfd7177894ca781f843
alvaroneto19 <81449169+alvaroneto19@users.noreply.github.com>
2021-12-20 21:30:05 -0300

    Create Livraria1.java

---
+++ Livraria1.java
@@ -0,0 +1,28 @@
+package biblioteca;
+
+import java.util.ArrayList;
+
+public class Livraria {
+
+    static String verificar(double começo, double fim) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String verificar(String tipo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean retirar(String titulo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getverlivros() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean ver() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+    
