de2fdf228a392478fad47ae9ed62221734fffeac
alvaroneto19 <81449169+alvaroneto19@users.noreply.github.com>
2021-12-20 21:28:19 -0300

    Create Exceção2.java

---
+++ Exceção2.java
@@ -0,0 +1,14 @@
+package biblioteca;
+
+/**
+ *
+ * @author 123
+ */
+public class EscolhalimitadaExceções extends Exception{
+    public String mensagem;
+     EscolhalimitadaExceções(){
+         
+            this.mensagem = "";
+        }
+
+}
